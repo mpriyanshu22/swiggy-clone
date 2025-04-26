a51badd44813ef04a284d7ed278251f60971c529
Priyanshu Mishra <iitianki22@gmail.com>
2025-04-26 21:22:26 +0530

    responsive

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,6 +1,6 @@
 function Shimmer() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10 w-[90%] mx-auto mt-20">
+    <div className="flex justify-center flex-row flex-wrap items-center gap-10 w-[100%] mx-auto mt-20 mx-auto">
       {[...Array(12)].map((_, index) => (
         <div key={index} className="h-72 w-72">
           <div className="w-70 h-45 rounded-xl bg-gray-400"></div>
